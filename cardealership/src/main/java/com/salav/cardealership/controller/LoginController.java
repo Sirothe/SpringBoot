6056c2979d48bd79b2fc4992d56d41210cdbea4a
Sirothe <58854544+Sirothe@users.noreply.github.com>
2021-04-26 12:46:45 +0200

    Spring Boot
    controller update

--- cardealership/src/main/java/com/salav/cardealership/controller/LoginController.java
+++ cardealership/src/main/java/com/salav/cardealership/controller/LoginController.java
@@ -3,10 +3,8 @@ package com.salav.cardealership.controller;
 import com.salav.cardealership.config.LoginCredentials;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.vault.core.VaultTemplate;
-import org.springframework.vault.support.VaultResponse;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Map;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
@@ -18,15 +16,5 @@ public class LoginController {
     @PostMapping("/login")
     public void login(@RequestBody LoginCredentials credentials) {
         //method to login
-    }
-
-    @GetMapping("/dupa")
-    public Object getCredentials() {
-        VaultResponse response = vaultTemplate.read("/secret/data/myapp");
-        if(response!=null) {
-            Object username = response.getData().get("data");
-            return username;
-        }
-        return null;
     }
 }
